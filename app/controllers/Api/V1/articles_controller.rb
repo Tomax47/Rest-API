ff4d57e4108b18e760d5ab3706196a5f7ed5db2c
root <tammam744@gmail.com>
2023-09-12 13:54:51 +0300

    Added the show & create methods

--- app/controllers/Api/V1/articles_controller.rb
+++ app/controllers/Api/V1/articles_controller.rb
@@ -5,8 +5,31 @@ module Api
 
             def index 
                 @articles = Article.all.order("created_at DESC")
-                render json: {status: 'SUCCESS', message: 'Loaded articles', data: @articles}
+                render json: {status: 'SUCCESS', message: 'Loaded articles', data: @articles},status: :ok
             end
+
+
+            def show 
+                @article = Article.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded article', data: @article},status: :ok
+            end
+
+            def create 
+                @article = Article.new(article_params)
+                
+                if @article.save
+                    render json: {status: 'SUCCESS', message: 'Saved article', data: @article},status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Article cant be saved', data: @article.errors},status: :unprocessable_entity
+                end
+            end
+
+            private 
+
+            def article_params 
+                params.require(:article).permit(:title, :body)
+            end
+
 
 
         end
